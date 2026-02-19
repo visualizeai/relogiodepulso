de69586447391c1b0bcc13a17958f4212f9e7b8f
visualizeai <limpidopedro+7@gmail.com>
2026-02-19 10:28:26 -0300

    Nexa Builde: update_file src/components/Container.tsx

---
+++ src/components/Container.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+export default function Container({ children }: { children: ReactNode }) {
+  return (
+    <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
+      {children}
+    </div>
+  );
+}
