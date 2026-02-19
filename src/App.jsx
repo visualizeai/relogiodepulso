3104efc4eebcdaf2593976c42b435276a024aa4a
visualizeai <limpidopedro+7@gmail.com>
2026-02-19 10:24:47 -0300

    init: src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App(){
+  return (
+    <div style={{padding:24, fontFamily:'system-ui'}}>
+      <h1>relogiodepulso</h1>
+      <p>Projeto criado automaticamente. Agora peça mudanças no chat 🙂</p>
+    </div>
+  )
+}
